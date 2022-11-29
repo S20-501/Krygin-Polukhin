4f1126ef8cc24fc1d5f7aa9cdf7102369482e223
ilya_polukhin <71003158+JustAEro@users.noreply.github.com>
2022-11-29 15:25:30 +0300

    Add files via upload

--- Lab2/DACControlModule.vhd
+++ Lab2/DACControlModule.vhd
@@ -24,72 +24,100 @@ end DACControlModule;
 
 architecture Behavioral of DACControlModule is
 	signal ClkReduceFreq_count: std_logic := '0';
-	signal IComponentGot: std_logic := '0';
-	signal DAC_Write_Buf: std_logic := '0';
+	signal DAC_Select_buf: std_logic := '0';
 	signal DAC_Rst_buf: std_logic := '0';
-	signal DAC_Rst_count: std_logic_vector(2 downto 0) := (others=>'0');
+	signal DAC_Rst_count: std_logic_vector(3 downto 0) := (others=>'0');
+	signal DAC_Clk_falling: std_logic := '0';
 	
 	
 begin
 	
-	process(ClkReduceFreq_count,DAC_Rst_buf)
+	process(Clk,DAC_Rst_buf,nRst)
 	begin
-		if (rising_edge(ClkReduceFreq_count)) then
+		if (nRst = '0') then
+			DAC_Rst_Count <= "0000";
+		elsif (rising_edge(Clk)) then
 			if (DAC_Rst_buf='1') then
 				DAC_Rst_count <= DAC_Rst_count + 1;
-			elsif (DAC_Rst_buf='0' and DAC_Rst_count < "100") then
-				DAC_Rst_Count <= "000";
+			elsif (DAC_Rst_buf='0' and DAC_Rst_count < "1000") then
+				DAC_Rst_Count <= "0000";
 			end if;
 		end if;
 	end process;
 	
 	
+	process(Clk,nRst,Power_Down,DAC_Rst_count)  -- 80 MHz -> 40 MHz
+	begin
+		if (nRst='0' or Power_Down='1' or DAC_Rst_count >= "1000") then
+			ClkReduceFreq_count <= '0';
+		elsif (rising_edge(Clk)) then
+			ClkReduceFreq_count <= not ClkReduceFreq_count;
+		end if;
+	end process;
+
+	
+	process(nRst,Rst_For_DAC)
+	begin
+		if (nRst='0' or Rst_For_DAC='1') then
+			DAC_Rst_buf <= '1';
+		else
+			DAC_Rst_buf <= '0';
+		end if;
+	end process;
 	
 	
-	process(Clk,nRst)  -- 80 MHz -> 40 MHz
+	
+	process(Clk,nRst,Power_Down,DAC_Rst_count)
+	begin
+		if (nRst='0' or Power_Down='1' or DAC_Rst_count >= "1000") then
+			DAC_Clk_falling <= '0';
+			
+		elsif (rising_edge(Clk)) then
+			DAC_Clk_falling <= not DAC_Clk_falling;
+			
+		end if;
+	
+	end process;
+	
+	
+	process(Clk,nRst,DAC_Select_buf,DAC_I_sig,DAC_Q_sig)
+	begin
+		if (nRst='0') then
+			DAC_Data <= "0000000000";
+		elsif (rising_edge(Clk)) then
+		
+			if (DAC_Select_buf='1') then
+				DAC_Data <= DAC_I_sig;
+			elsif (DAC_Select_buf='0') then
+				DAC_Data <= DAC_Q_sig;
+			end if;
+			
+		end if;
+	end process;
+	
+	
+	process(Clk,nRst,DAC_Clk_falling) 
 	begin
 	
 		if (nRst='0') then
-			DAC_Rst_buf <= '1';	
-			DAC_Write_buf <= '0';
-			DAC_Data <= "0000000000";
-			DAC_Select <= '0';
-			ClkReduceFreq_count <= '0';
-			IComponentGot <= '0';
+			DAC_Select_buf <= '0';
 			
-		elsif (Power_Down='1' or DAC_Rst_count >= "100" or Rst_For_DAC='1') then
-			DAC_Rst_buf <= '1';
-			DAC_Write_buf <= '0';
-			DAC_Data <= "0000000000";
-			DAC_Select <= '0';
-			ClkReduceFreq_count <= '0';
-		
 		elsif (rising_edge(Clk)) then
-				ClkReduceFreq_count <= not ClkReduceFreq_count;
-				IComponentGot <= not IComponentGot;
-				DAC_Write_buf <= not DAC_Write_buf;
-	
-		elsif (falling_edge(Clk)) then
-				if (DAC_Q_sig="0000000000" or IComponentGot='0') then
-					DAC_Select <= '1';  --route to I DAC
-					DAC_Data <= DAC_I_sig;
-				
-				elsif (IComponentGot='1') then
-						DAC_Select <= '0';
-						DAC_Data <= DAC_Q_sig;
+				if (DAC_Clk_falling = '1') then  --falling edge of DAC_Clk
+					DAC_Select_buf <= not DAC_Select_buf;  --route to I DAC
 				end if;
-				
 			
 		end if;
 	end process;
 	
 	
 	
-	DAC_signals_assignment : process(ClkReduceFreq_count)
+	DAC_signals_assignment : process(ClkReduceFreq_count,DAC_Rst_buf,DAC_Select_buf)
 	begin
 		DAC_Clk <= ClkReduceFreq_count;
 		DAC_Rst <= DAC_Rst_buf;
-		DAC_Write <= DAC_Write_Buf;
+		DAC_Write <= ClkReduceFreq_count;
+		DAC_Select <= DAC_Select_buf;
 	end process;
 	
 	
